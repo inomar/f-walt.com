7611ff9909e49803210be4de6bd12c0802d39fd0
MakotoInomata <inomar1486@gmail.com>
2019-06-06 12:45:09 +0900

    fix head mbile

--- frontend/components/atoms/Heading.js
+++ frontend/components/atoms/Heading.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import media from 'styled-media-query';
+
 import { fontFamilyAntiqu } from './snippets';
 
 const Title = styled.h2`
@@ -6,6 +8,9 @@ const Title = styled.h2`
   text-align: center;
   font-size: 2rem;
   margin: 10px 0 20px;
+  ${media.lessThan('medium')`
+    margin: 0 0 10px;
+  `};
 `;
 
 const Heading = ({title}) => (
