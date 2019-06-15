4978cedb672d4b2986d8e2aa6ceee1727397f76c
MakotoInomata <inomar1486@gmail.com>
2019-06-15 15:26:35 +0900

    add _app.js

---
+++ frontend/pages/_app.js
@@ -0,0 +1,26 @@
+import App, { Container } from 'next/app';
+import React from 'react';
+import { PageTransition } from 'next-page-transitions';
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <Container>
+        <PageTransition timeout={300} classNames="page-transition">
+          <Component {...pageProps} />
+        </PageTransition>
+      </Container>
+    )
+  }
+}
